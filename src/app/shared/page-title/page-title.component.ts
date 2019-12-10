f09f11f6e0213a529f5ac4efcaeb6c178254f283
Aguara Gazu <aguara_gazu@yahoo.com.ar>
2019-12-10 06:14:44 +0000

    fixed to prod

--- src/app/shared/page-title/page-title.component.ts
+++ src/app/shared/page-title/page-title.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class PageTitleComponent implements OnInit {
 
   constructor() { }
+  pageTitle: string;
+  pageImage: string;
 
   ngOnInit() {
   }
